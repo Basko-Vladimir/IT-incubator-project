082d819b5f388f0ccb0c3c085d050bec006337b4
BaVlaG <1989bvg@gmail.com>
2020-02-24 21:17:14 +0300

    Create project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div>one div</div>
+            <div>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
