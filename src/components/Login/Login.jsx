123ce22d3df29362cd6954211ebc57a7ccb671eb
Basko-Vladimir <1989bvg@gmail.com>
2020-03-07 09:49:28 +0300

    Initial style design components Login and Navbar

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,11 +4,11 @@ import styles from "./Login.module.css";
 const Login = () => {
     return (
         <div className={styles.login}>
-            <form>
-                <span>Authorization</span>
-                <input type='text' />
-                <input type='text' />
-                <input type='submit'/>
+            <form className={styles.loginForm}>
+                <span className={styles.formHeader}>Authorization</span>
+                <input type='text' className={styles.inputs} placeholder='E-mail'/>
+                <input type='text' className={styles.inputs} placeholder='Password'/>
+                <input type='submit' value='Enter' className={styles.submitBtn}/>
             </form>
         </div>
     )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,26 @@ import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-      <div className={styles.navBar}>
-          Navbar
-      </div>
+      <nav className={styles.navBar}>
+          <div className={styles.navItem}>
+              <a href="#">Profile</a>
+          </div>
+          <div className={styles.navItem}>
+              <a href="#">News</a>
+          </div>
+          <div className={styles.navItem}>
+              <a href="#">Messages</a>
+          </div>
+          <div className={styles.navItem}>
+              <a href="#">Users</a>
+          </div>
+          <div className={styles.navItem}>
+              <a href="#">Music</a>
+          </div>
+          <div className={styles.navItem}>
+              <a href="#">Settings</a>
+          </div>
+      </nav>
   )
 };
 
