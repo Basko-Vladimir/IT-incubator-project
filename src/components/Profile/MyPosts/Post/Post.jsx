f65f10a294114a91adf25061b48df926f082dba5
Basko-Vladimir <1989bvg@gmail.com>
2020-03-13 21:59:07 +0300

    Create component Post

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import styles from "./Post.module.css";
+import avatar from "../../../../images/avatar.jpg";
 
 const Post = () => {
     return(
         <div className={styles.postBlock}>
             <div className={styles.postHeader}>
-
+                <img className={styles.avatar} src={avatar} alt='avatar'/>
+                <div className={styles.name}>
+                    <span>Иван Иванов</span>
+                </div>
+                <div className={styles.time}>
+                    <span>5 min ago</span>
+                </div>
             </div>
-
+            <div className={styles.postMessage}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et labore libero
+                maxime numquam quidem, veniam. Aliquam aspernatur aut consequatur cupiditate.
+            </div>
+            <div className={styles.postReview}>
+                <div>
+                    <img src="" alt="like"/>
+                    <span>37</span>
+                </div>
+                <div>
+                    <img src="" alt="comment"/>
+                </div>
+            </>
         </div>
     )
 };
