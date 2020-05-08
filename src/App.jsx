839028a4e023daa279c58573dc0fdfa6d4300e0e
Basko-Vladimir <1989bvg@gmail.com>
2020-05-08 22:53:09 +0300

    Realization lazy import and suspense with error:(

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
@@ -7,7 +7,7 @@ import {Route, withRouter} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import Dialogs from "./components/Dialogs/Dialogs";
+// import Dialogs from "./components/Dialogs/Dialogs";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import LoginFormContainer from "./components/Login/LoginForm/LoginFormContainer";
@@ -17,12 +17,15 @@ import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 
+const Dialogs = lazy(import("./components/Dialogs/Dialogs"));
+
 class App extends React.Component {
     componentDidMount() {
         this.props.initializeApp();
     }
+
     render() {
-        if (!this.props.initialized){
+        if (!this.props.initialized) {
             return <Preloader/>
         }
         return (
@@ -31,14 +34,18 @@ class App extends React.Component {
                 <LoginFormContainer/>
                 <Navbar/>
                 <div className='mainBlockWrap'>
-                    <Route path={'/login'} render={() => <Login/>}/>
-                    <Route path='/home' render={() => <Main/>}/>
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                    <Route path='/news' render={() => <News/>}/>
-                    <Route path='/dialogs' render={() => <Dialogs/>}/>
-                    <Route path='/users' render={() => <UsersContainer/>}/>
-                    <Route path='/music' render={() => <Music/>}/>
-                    <Route path='/settings' render={() => <Settings/>}/>
+                        <Route path={'/login'} render={() => <Login/>}/>
+                        <Route path='/home' render={() => <Main/>}/>
+                        <Route path='/profile/:userId?' render={() => <ProfileContainer/> }/>
+                        <Route path='/news' render={() => <News/>}/>
+                        <Route path='/dialogs' render={() => {
+                            return <Suspense fallback={<Preloader/>}>
+                                    <Dialogs/>
+                            </Suspense>
+                        }}/>
+                        <Route path='/users' render={() => <UsersContainer/>}/>
+                        <Route path='/music' render={() => <Music/>}/>
+                        <Route path='/settings' render={() => <Settings/>}/>
                 </div>
             </div>
         );
